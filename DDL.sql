d6d189790ab67583718d3210add17a8cfd730f33
lin231135 <131785834+lin231135@users.noreply.github.com>
2025-03-24 17:38:13 -0600

    creación de tipos y tablas de productos e inventario

--- DDL.sql
+++ DDL.sql
@@ -57,3 +57,33 @@ CREATE TABLE Shift (
     status Shift_Status,
     FOREIGN KEY (employee_id) REFERENCES Employee(employee_id)
 );
+
+CREATE TYPE Movement_Type_Name AS ENUM ('incoming', 'outgoing', 'adjustment');
+
+-- Tabla de Categoría de Productos
+CREATE TABLE Product_Category (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL UNIQUE
+);
+
+-- Tabla de Productos
+CREATE TABLE Product (
+    product_id SERIAL PRIMARY KEY,
+    category_id INTEGER NOT NULL,
+    name VARCHAR NOT NULL,
+    description TEXT,
+    purchase_price FLOAT CHECK (purchase_price >= 0),
+    sale_price FLOAT CHECK (sale_price >= 0),
+    batch VARCHAR,
+    expiration_date TIMESTAMP,
+    is_perishable BOOLEAN,
+    FOREIGN KEY (category_id) REFERENCES Product_Category(category_id)
+);
+
+CREATE INDEX idx_product_name ON Product(name);
+
+-- Tabla de Tipos de Movimiento
+CREATE TABLE Movement_Type (
+    movement_type_id SERIAL PRIMARY KEY,
+    type_name Movement_Type_Name
+);
