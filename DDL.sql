f2aa75c5d3317aa14908c5a0d3ba1e62275275bc
amb231143 <131787767+Dulce2004@users.noreply.github.com>
2025-03-24 17:29:15 -0600

    Roles y permisos
    Creación de tabla de Roles y Permisos

--- DDL.sql
+++ DDL.sql
@@ -11,3 +11,25 @@ CREATE TABLE Users (
 );
 
 CREATE INDEX idx_user_email ON Users(email);
+
+-- Tabla de Roles
+CREATE TABLE Role (
+    role_id SERIAL PRIMARY KEY,
+    role_name VARCHAR NOT NULL UNIQUE
+);
+
+-- Tabla de Permisos
+CREATE TABLE Permission (
+    permission_id SERIAL PRIMARY KEY,
+    permission_name VARCHAR NOT NULL,
+    description VARCHAR
+);
+
+-- Tabla de Relación Rol-Permiso
+CREATE TABLE Role_Permission (
+    role_permission_id SERIAL PRIMARY KEY,
+    role_id INTEGER NOT NULL,
+    permission_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES Role(role_id),
+    FOREIGN KEY (permission_id) REFERENCES Permission(permission_id)
+);
